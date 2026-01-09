1903ad0de2275363f41aa6532adc95ce71e8c9dd
israeldah <idah@byupathway.edu>
2026-01-09 15:39:27 +0000

    Home Page W01 Assignment wdd131

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+
+const year = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").innerHTML = "Last modification: " + document.lastModified;
