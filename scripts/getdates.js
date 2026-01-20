a0c209f5d4f634bd91c61d8dfdbee9521745421f
israeldah <idah@byupathway.edu>
2026-01-20 15:21:22 +0000

    Bom app completed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,5 @@ const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 
 document.getElementById("lastModified").innerHTML = "Last modification: " + document.lastModified;
+
+
