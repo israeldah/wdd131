6b147674e19eb83923bd89a60e6aef7805885f23
israeldah <idah@byupathay.edu>
2026-01-30 16:17:33 +0000

    completed all country activities

--- scripts/place.js
+++ scripts/place.js
@@ -1,15 +1,29 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
 
-const year = new Date().getFullYear();
-
-document.getElementById("currentyear").textContent = year;
-
-document.getElementById("lastModified").innerHTML = "Last modification: " + document.lastModified;
+document.getElementById("lastModified").textContent =
+    "Last modification: " + document.lastModified;
 
 
-const hamButton = document.querySelector("#menu");
-const navigation = document.querySelector("nav");
+const temperature = 28;
+const windSpeed = 8;
 
-hamButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-    hamButton.classList.toggle("open");
-});
+
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
+    ).toFixed(1);
+}
+
+
+let windChill = "N/A";
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill = `${calculateWindChill(temperature, windSpeed)} °C`;
+}
+
+document.getElementById("windchill").textContent = windChill;
+
