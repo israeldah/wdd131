6db00979a01b326a4d9e1c1a938e8e9778bfcd4f
israeldah <idah@byupathay.edu>
2026-02-05 22:41:33 +0000

    siteplan beginning project

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").innerHTML = "Last modification: " + document.lastModified;
